84cf827f828e7dff9bb47634c60947632b11e8aa
moorcroftg <213037513@mycput.ac.za>
2017-03-11 19:36:53 +0200

    This is a commit. I think

--- src/main/java/com/helloworld/App.java
+++ src/main/java/com/helloworld/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        alert("Hi there");
+        System.out.println( "Hello World! This is definitely something!!" );
     }
 }
